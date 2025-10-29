23354511f5f4ce437cfc0882df493f3daaa62510
IvanRRiadhy <ivanrifqi1002@gmail.com>
2025-10-29 11:31:56 +0700

    29/10/2025

--- main/src/components/forms/theme-elements/CustomFormLabel.tsx
+++ main/src/components/forms/theme-elements/CustomFormLabel.tsx
@@ -1,17 +1,28 @@
-'use client'
+'use client';
 
 import { styled } from '@mui/material/styles';
 import { Typography } from '@mui/material';
 
-const CustomFormLabel = styled((props: any) => (
-  <Typography
-    variant="subtitle1"
-    fontWeight={600}
-    {...props}
-    component="label"
-    htmlFor={props.htmlFor}
-  />
-))(() => ({
+interface CustomFormLabelProps {
+  htmlFor?: string;
+  error?: boolean;
+  children?: React.ReactNode;
+}
+
+const CustomFormLabel = styled(
+  ({ error, htmlFor, children, ...rest }: CustomFormLabelProps) => (
+    <Typography
+      variant="subtitle1"
+      fontWeight={600}
+      component="label"
+      htmlFor={htmlFor}
+      color={error ? 'error.main' : 'text.primary'}
+      {...rest}
+    >
+      {children}
+    </Typography>
+  )
+)(() => ({
   marginBottom: '5px',
   marginTop: '25px',
   display: 'block',
